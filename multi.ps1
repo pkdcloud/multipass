fb0d6f95a42e4b1fa4bfe2956b5453c2a48e6455
Paul Devine <paul@pkd.cloud>
2022-02-19 20:44:19 +1100

    added additional commands

--- multi.ps1
+++ multi.ps1
@@ -1,6 +1,18 @@
 # multi.ps1
 
-multipass delete --all
-multipass purge
-multipass launch --name ubuntu-1 --cloud-init .\config\cloud-init\ubuntu-latest.yaml
-multipass shell ubuntu-1
+$COMMAND=$args[0]
+$MACHINE_NAME=$args[1]
+$CLOUD_INIT_SCRIPT=$args[2]
+
+switch ($COMMAND)
+{
+    "launch" {
+		multipass delete --all
+		multipass purge
+		multipass launch --name $MACHINE_NAME --cloud-init .\config\cloud-init\ubuntu-latest.yaml
+		multipass shell $MACHINE_NAME
+	}
+    "shell" {
+		multipass shell $MACHINE_NAME
+	}
+}
