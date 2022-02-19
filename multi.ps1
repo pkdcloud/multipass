94f2cccc094a45d74b842aa226d6e8263cbfdd17
Paul Devine <paul@pkd.cloud>
2022-02-20 00:15:32 +1100

    replacing ssh

--- multi.ps1
+++ multi.ps1
@@ -8,8 +8,9 @@ switch ($COMMAND)
 {
     "launch" {
 		ssh-keygen -t rsa -b 4096 -f $env:USERPROFILE/.ssh/multipass -q -N '""'
-		(Get-Content -path .\config\cloud-init\ubuntu-latest.yaml -Raw) -replace 'ssh-public-key', (Get-Content -path $env:USERPROFILE/.ssh/multipass.pub -Raw)
+		(Get-Content -path .\config\cloud-init\ubuntu-latest.yaml -Raw).replace('ssh-public-key', (Get-Content -path $env:USERPROFILE/.ssh/multipass.pub -Raw)) | Set-Content .\config\cloud-init\ubuntu-latest.yaml
 		multipass launch -c 4 -m 8192M -vvvv --name $MACHINE_NAME --cloud-init .\config\cloud-init\ubuntu-latest.yaml
+		(Get-Content -path .\config\cloud-init\ubuntu-latest.yaml -Raw).replace((Get-Content -path $env:USERPROFILE/.ssh/multipass.pub -Raw), 'ssh-public-key') | Set-Content .\config\cloud-init\ubuntu-latest.yaml
 	}
     "shell" {
 		multipass shell $MACHINE_NAME
