84ade8774592618688ccbd5ed156089871c50d8e
Paul Devine <paul@pkd.cloud>
2022-02-19 23:09:08 +1100

    quiter unzip

--- multi.ps1
+++ multi.ps1
@@ -7,16 +7,14 @@ $CLOUD_INIT_SCRIPT=$args[2]
 switch ($COMMAND)
 {
     "launch" {
-		#rm $env:USERPROFILE/.ssh/multipass-$MACHINE_NAME
-		#rm $env:USERPROFILE/.ssh/multipass-$MACHINE_NAME.pub
 		ssh-keygen -t rsa -b 4096 -f $env:USERPROFILE/.ssh/multipass-$MACHINE_NAME -q -N '""'
-		multipass launch -c 4 -m 8192M -v --name $MACHINE_NAME --cloud-init .\config\cloud-init\ubuntu-latest.yaml
-		multipass shell $MACHINE_NAME
+		multipass launch -c 4 -m 8192M -vvvv --name $MACHINE_NAME --cloud-init .\config\cloud-init\ubuntu-latest.yaml
 	}
     "shell" {
 		multipass shell $MACHINE_NAME
 	}
     "clean" {
+		rm $env:USERPROFILE/.ssh/multipass*
 		multipass delete --all
 		multipass purge
 	}
